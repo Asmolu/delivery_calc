9c89f8fc3871e776a3c5b98d8139a9c4b5a8078a
Asmolu <misyurevdd@gmail.com>
2025-10-26 19:52:55 +0300

    prepare for Render deployment

--- main.py
+++ main.py
@@ -386,3 +386,7 @@ def root():
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+from fastapi.staticfiles import StaticFiles
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
